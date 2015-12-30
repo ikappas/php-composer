ba5ee69195cb34b62cab74688e491121fa89f3df
Ioannis Kappas <ikappas@devworks.gr>
2015-12-30 13:24:30 +0200

    rework json file spec

--- spec/lib/composer/json/json_file_spec.rb
+++ spec/lib/composer/json/json_file_spec.rb
@@ -1,185 +1,172 @@
 require_relative '../../../spec_helper'
 
-describe JsonFile do
+describe ::Composer::Json::JsonFile do
 
-  it 'parse error detect extra comma' do
-    json = '{
-        "foo": "bar",
-    }'
-    expect { JsonFile.parse_json(json) }.to raise_error(JSON::ParserError)
+  subject(:json_file) { described_class }
+
+  context '#parse_json' do
+
+    [
+      {
+        name: 'detects extra comma',
+        json: "{\n  \"foo\": \"bar\",\n}"
+      },
+      {
+        name: 'detects extra comma in array',
+        json: "{\n  \"foo\": [\n    \"bar\",\n  ]\n}"
+      },
+      {
+        name: 'detects unescaped backslash',
+        json: "{\n  \"fo\\o\": \"bar\",\n}"
+      },
+      {
+        name: 'skips escaped backslash',
+        json: "{\n  \"fo\\\\o\": \"bar\"\n  \"a\": \"b\"\n}"
+      },
+      {
+        name: 'detects single quotes',
+        json: "{\n  'foo': \"bar\"\n}"
+      },
+      {
+        name: 'detects missing quotes',
+        json: "{\n  foo: \"bar\"\n}"
+      },
+      {
+        name: 'detects array as hash',
+        json: "{\n  foo: [\"bar\": \"baz\"]\n}"
+      },
+      {
+        name: 'detects missing comma',
+        json: "{\n  \"foo\": \"bar\"\n  \"bar\": \"foo\"\n}"
+      },
+      {
+        name: 'detects missing comma multi-line',
+        json: "{\n  \"foo\": \"bar\"\n\n  \"bar\": \"foo\"\n}"
+      },
+      {
+        name: 'detects missing colon',
+        json: "{\n  \"foo\": \"bar\",\n  \"bar\" \"foo\"\n}"
+      },
+
+    ].each do |test|
+      it "raises error when it #{test[:name]}" do
+        expect { described_class::parse_json(test[:json]) }.to raise_error(::JSON::ParserError)
+      end
+    end
+
   end
 
-  it 'parse error detect extra comma in array' do
-    json = '{
-      "foo": [
-        "bar",
-      ]
-    }'
-    expect { JsonFile.parse_json(json) }.to raise_error(JSON::ParserError)
+  context '#encode' do
+
+    [
+      {
+        name: 'simple json string',
+        data: { 'name' => 'composer/composer' },
+        expected: "{\n    \"name\": \"composer/composer\"\n}"
+      },
+      {
+        name: 'trailing backslash',
+        data: { 'Metadata\\' => 'src/' },
+        expected: "{\n    \"Metadata\\\\\": \"src/\"\n}"
+      },
+      {
+        name: 'empty array',
+        data: { 'test' => [], 'test2' => {} },
+        expected: "{\n    \"test\": [],\n    \"test2\": {}\n}"
+      },
+      {
+        name: 'escape',
+        data: { "Metadata\\\"" => 'src/' },
+        expected: "{\n    \"Metadata\\\\\\\"\": \"src/\"\n}"
+      },
+      {
+        name: 'unicode',
+        data: { "Žluťoučký \" kůň" => "úpěl ďábelské ódy za €" },
+        expected: "{\n    \"Žluťoučký \\\" kůň\": \"úpěl ďábelské ódy za €\"\n}"
+      },
+      {
+        name: 'only unicode',
+        data: %{\\/ƌ},
+        options: described_class::JSON_UNESCAPED_UNICODE,
+        expected: %{"\\\\/ƌ"}
+      },
+      {
+        name: 'escaped slashes',
+        data: %{\\/foo},
+        options: 0,
+        expected: %{"\\\\/foo"}
+      },
+      {
+        name: 'escaped backslashes',
+        data: %{a\\b},
+        options: 0,
+        expected: %{"a\\\\b"}
+      },
+      {
+        name: 'escaped unicode',
+        data: 'ƌ',
+        options: 0,
+        expected: %{"\u018c"}
+      },
+
+    ].each do |test|
+      it "succeeds on #{test[:name]}" do
+        if test[:options]
+          expect(described_class::encode(test[:data], test[:options])).to be == test[:expected]
+        else
+          expect(described_class::encode(test[:data])).to be == test[:expected]
+        end
+      end
+    end
+
+    it 'succeeds on double escaped unicode' do
+      data = ["Zdjęcia","hjkjhl\u0119kkjk"]
+      encoded_data = described_class::encode(data)
+      double_encoded_data = described_class::encode({ 't' => encoded_data })
+      decoded_data = JSON::parse(double_encoded_data)
+      double_data = JSON::parse(decoded_data['t'])
+      expect(double_data).to be == data
+    end
+
   end
 
-  # it 'parse error detect unescaped backslash' do
-  #   json = '{
-  #     "fo\o": "bar"
-  #   }'
-  #   expect { JsonFile.parse_json(json) }.to raise_error(JSON::ParserError)
-  # end
-
-  it 'parse error skips escaped backslash' do
-    json = '{
-      "fo\\\\o": "bar"
-      "a": "b"
-    }'
-    expect { JsonFile.parse_json(json) }.to raise_error(JSON::ParserError)
-    # JsonFileHelpers.expect_parse_exception("", json)
-    # $this->expectParseException('Parse error on line 2', json)
-  end
-
-  it 'parse error detect single quotes' do
-    json = '{
-      \'foo\': "bar"
-    }'
-    expect { JsonFile.parse_json(json) }.to raise_error(JSON::ParserError)
-    # $this->expectParseException('Parse error on line 1', json)
-  end
-
-  it 'parse error detect missing quotes' do
-    json = '{
-      foo: "bar"
-    }'
-    expect { JsonFile.parse_json(json) }.to raise_error(JSON::ParserError)
-    # $this->expectParseException('Parse error on line 1', json)
-  end
-
-  it 'parse error detect array as hash' do
-    json = '{
-      "foo": ["bar": "baz"]
-    }'
-    expect { JsonFile.parse_json(json) }.to raise_error(JSON::ParserError)
-    # $this->expectParseException('Parse error on line 2', json)
-  end
-
-  it 'parse error detect missing comma' do
-    json = '{
-      "foo": "bar"
-      "bar": "foo"
-    }'
-    expect { JsonFile.parse_json(json) }.to raise_error(JSON::ParserError)
-    # $this->expectParseException('Parse error on line 2', json)
-  end
-
-  it 'schema validation' do
-    json = Composer::Json::JsonFile.new(
-      File.expand_path("../../../../fixtures/composer.json", __FILE__)
-    )
-    expect(json.validate_schema).to be_truthy
-  end
-
-  it 'parse error detect missing comma multiline' do
-    json = '{
-      "foo": "barbar"
-
-      "bar": "foo"
-    }'
-    expect { JsonFile.parse_json(json) }.to raise_error(JSON::ParserError)
-    # $this->expectParseException('Parse error on line 2', json)
-  end
-
-  it 'ParseErrorDetectMissingColon' do
-    json = '{
-      "foo": "bar",
-      "bar" "foo"
-    }'
-    expect { JsonFile.parse_json(json) }.to raise_error(JSON::ParserError)
-    # $this->expectParseException('Parse error on line 3', json)
-  end
-
-  it 'SimpleJsonString' do
-    data = { 'name' => 'composer/composer' }
-    json = "{\n    \"name\": \"composer/composer\"\n}"
-    expect(JsonFile::encode(data)).to be == json
-  end
-
-  it 'TrailingBackslash' do
-    data = { 'Metadata\\' => 'src/' }
-    json = "{\n    \"Metadata\\\\\": \"src/\"\n}"
-    expect(JsonFile::encode(data)).to be == json
-  end
-
-  it 'FormatEmptyArray' do
-    data = { 'test' => [], 'test2' => {} }
-    json = "{\n    \"test\": [],\n    \"test2\": {}\n}"
-    expect(JsonFile::encode(data)).to be == json
-  end
-
-  it 'Escape' do
-    data = { "Metadata\\\"" => 'src/' }
-    json = "{\n    \"Metadata\\\\\\\"\": \"src/\"\n}"
-    expect(JsonFile::encode(data)).to be == json
-  end
-
-  it 'Unicode' do
-    data = { "Žluťoučký \" kůň" => "úpěl ďábelské ódy za €" }
-    json = "{\n    \"Žluťoučký \\\" kůň\": \"úpěl ďábelské ódy za €\"\n}"
-    expect(JsonFile::encode(data)).to be == json
-  end
-
-  it 'OnlyUnicode' do
-    data = %{\\/ƌ}
-    json = %{"\\\\/ƌ"}
-    # $this->assertJsonFormat('"\\\\\\/ƌ"', data, JsonFile::JSON_UNESCAPED_UNICODE)
-    options = JsonFormatter::JSON_UNESCAPED_UNICODE
-    expect(JsonFile::encode(data, options)).to be == json
-  end
-
-  it 'EscapedSlashes' do
-    data = %{\\/foo}
-    json = %{"\\\\/foo"}
-    options = 0
-    expect(JsonFile::encode(data, options)).to be == json
-    # $this->assertJsonFormat('"\\\\\\/foo"', data, 0)
-  end
-
-  it 'EscapedBackslashes' do
-    data = %{a\\b}
-    json = %{"a\\\\b"}
-    options = 0
-    expect(JsonFile::encode(data, options)).to be == json
-    # $this->assertJsonFormat('"a\\\\b"', data, 0)
-  end
-
-  it 'EscapedUnicode' do
-    data = 'ƌ'
-    json = %{"\u018c"}
-    options = 0
-    expect(JsonFile::encode(data, options)).to be == json
-    # $this->assertJsonFormat('"\\u018c"', data, 0)
-  end
-
-  it 'DoubleEscapedUnicode' do
-    # jsonFile = new JsonFile('composer.json')
-    data = ["Zdjęcia","hjkjhl\u0119kkjk"]
-    encoded_data = JsonFile::encode(data)
-    double_encoded_data = JsonFile.encode({ 't' => encoded_data })
-    decoded_data = JSON::parse(double_encoded_data)
-    double_data = JSON::parse(decoded_data['t'])
-    expect(double_data).to be == data
-  end
-
-  it '#write succeeds' do
-    data = { "test" => "test" }
-    jsonFile = JsonFile.new(
+  context '#write' do
+    it 'succeeds' do
+      data = { "test" => "test" }
+      jsonFile = described_class.new(
         File.expand_path('../../../../fixtures/test.json', __FILE__)
-    )
-    jsonFile.write(data)
+      )
+      jsonFile.write(data)
+    end
   end
 
-  it '#read succeeds' do
-    data = { "test" => "test" }
-    jsonFile = JsonFile.new(
+  context '#read' do
+    it 'succeeds' do
+      data = { "test" => "test" }
+      jsonFile = described_class.new(
         File.expand_path('../../../../fixtures/test.json', __FILE__)
-    )
-    read_data = jsonFile.read
-    expect(read_data).to be == data
+      )
+      read_data = jsonFile.read
+      expect(read_data).to be == data
+    end
   end
+
+  context '#validate_schema' do
+
+    it 'returns true on valid schema' do
+      json = described_class.new(
+        File.expand_path('../../../../fixtures/composer.json', __FILE__)
+      )
+      expect(json.validate_schema).to be_truthy
+    end
+
+    it 'raises error on invalid schema' do
+      json = described_class.new(
+        File.expand_path('../../../../fixtures/test.json', __FILE__)
+      )
+      expect{ json.validate_schema }.to raise_error(::Composer::Json::JsonValidationError)
+    end
+
+  end
+
 end
