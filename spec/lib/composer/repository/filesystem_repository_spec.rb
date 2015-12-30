f6dc1d6b2833bb7a5f3e5171d24799127fc60056
Ioannis Kappas <ikappas@devworks.gr>
2015-12-30 13:26:26 +0200

    rework filesystem repository spec

--- spec/lib/composer/repository/filesystem_repository_spec.rb
+++ spec/lib/composer/repository/filesystem_repository_spec.rb
@@ -1,14 +1,20 @@
 require_relative '../../../spec_helper'
 
-describe FilesystemRepository do
+describe ::Composer::Repository::FilesystemRepository do
+
+  let(:json_file) do
+    @json_file ||= begin
+      json_file = double(::Composer::Json::JsonFile)
+      allow(json_file).to receive(:is_a?).once.and_return( true )
+      json_file
+    end
+  end
+
+  let(:repository) { @repository ||= described_class.new(json_file) }
 
   it '#read succeeds' do
-    json = double(Composer::Json::JsonFile)
-    allow(json).to receive(:is_a?).once.and_return( true )
-    repo = FilesystemRepository.new(json)
-
-    expect(json).to receive(:exists?).once.and_return( true )
-    expect(json).to receive(:read).once.and_return([
+    expect(json_file).to receive(:exists?).once.and_return( true )
+    expect(json_file).to receive(:read).once.and_return([
       {
         'name' => 'package1',
         'version'=> '1.0.0-beta',
@@ -16,60 +22,46 @@ describe FilesystemRepository do
       }
     ])
 
-    packages = repo.packages
-
+    packages = repository.packages
     expect(packages.length).to be == 1
     expect(packages[0].name).to be == 'package1'
     expect(packages[0].version).to be == '1.0.0.0-beta'
     expect(packages[0].type).to be == 'vendor'
-
   end
 
-  it '#packages with corrupted repository file' do
-    json = double(Composer::Json::JsonFile)
-    allow(json).to receive(:is_a?).once.and_return( true )
+  context '#packages' do
 
-    repo = FilesystemRepository.new(json)
-    expect(json).to receive(:read).once.and_return( 'foo' )
-    expect(json).to receive(:exists?).once.and_return( true )
-    expect(json).to receive(:path).once.and_return( 'test\path' )
+    context 'with corrupted repository file' do
+      it 'raises invalid repository error' do
+        expect(json_file).to receive(:read).once.and_return( 'foo' )
+        expect(json_file).to receive(:exists?).once.and_return( true )
+        expect(json_file).to receive(:path).once.and_return( 'test\path' )
+        expect { repository.packages }.to raise_error(::Composer::InvalidRepositoryError)
+      end
+    end
 
-    expect { repo.packages }.to raise_error(Composer::InvalidRepositoryError)
-  end
-
-  it '#packages with non-existent repository file' do
-    json = double(Composer::Json::JsonFile)
-    allow(json).to receive(:is_a?).once.and_return( true )
-
-    repo = FilesystemRepository.new(json)
-    expect(json).to receive(:exists?).once.and_return( false )
-    expect(repo.packages).to be == []
+    context 'with non-existent repository file' do
+      it 'returns empty array' do
+        expect(json_file).to receive(:exists?).once.and_return( false )
+        expect(repository.packages).to be == []
+      end
+    end
   end
 
   it '#write succeeds' do
-    json = double(Composer::Json::JsonFile)
-    allow(json).to receive(:is_a?).once.and_return( true )
-
-    repo = FilesystemRepository.new(json)
-
-    expect(json).to receive(:read).once.and_return([])
-    expect(json).to receive(:exists?).once.and_return( true )
-    expect(json).to receive(:write).once.with([
+    expect(json_file).to receive(:read).once.and_return([])
+    expect(json_file).to receive(:exists?).once.and_return( true )
+    expect(json_file).to receive(:write).once.with([
       { 'name' => 'mypkg', 'type' => 'library', 'version' => '0.1.10', 'version_normalized' => '0.1.10.0' }
     ])
 
-    repo.add_package(self.get_package('mypkg', '0.1.10'))
-    repo.write
+    repository.add_package(build(:package, name: 'mypkg', version: '0.1.10'))
+    repository.write
   end
 
   it '#reload succeeds' do
-
-    json = double(Composer::Json::JsonFile)
-    allow(json).to receive(:is_a?).once.and_return( true )
-    repo = FilesystemRepository.new(json)
-
-    expect(json).to receive(:exists?).twice.and_return( true )
-    expect(json).to receive(:read).twice.and_return([
+    expect(json_file).to receive(:exists?).twice.and_return( true )
+    expect(json_file).to receive(:read).twice.and_return([
       {
         'name' => 'package1',
         'version'=> '1.0.0-beta',
@@ -77,9 +69,9 @@ describe FilesystemRepository do
       }
     ])
 
-    packages1 = repo.packages
-    repo.reload
-    packages2 = repo.packages
+    packages1 = repository.packages
+    repository.reload
+    packages2 = repository.packages
 
     expect(packages2.length).to be ==  packages1.length
     expect(packages2[0].name).to be == packages1[0].name
